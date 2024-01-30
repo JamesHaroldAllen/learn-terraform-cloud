e4dde35bdac1c552280eee154b0a082c9814f7c6
JamesHaroldAllen <157725507+JamesHaroldAllen@users.noreply.github.com>
2024-01-30 13:30:02 -0500

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "JHA-LearnTerraform"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
